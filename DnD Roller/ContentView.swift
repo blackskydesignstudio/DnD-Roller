a8a796e17fa54870d1f59bdf28c42896f7d10e3e
dk <none@gmail.com>
2019-10-09 14:34:32 -0700

    Initial Commit

---
+++ DnD Roller/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DnD Roller
+//
+//  Created by airMac on 2019-10-09.
+//  Copyright © 2019 Desmand King. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
